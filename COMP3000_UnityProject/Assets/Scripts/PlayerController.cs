d0155037134face94f6dc5acae69fbb433377236
Josie Wood <josieeewood@gmail.com>
2023-01-25 20:04:43 +0000

    Isometric player movement
    Very basic iso movement
    still need to implement animations for each principal direction

--- COMP3000_UnityProject/Assets/Scripts/PlayerController.cs
+++ COMP3000_UnityProject/Assets/Scripts/PlayerController.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    
 
     //variables
     public float moveSpeed = 1f;
+    public Rigidbody2D playerRB;
+    public Vector2 currentPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRB= GetComponent<Rigidbody2D>();
+        currentPos = GetComponent<Transform>().position;
     }
 
     // Update is called once per frame
@@ -19,4 +23,17 @@ public class PlayerController : MonoBehaviour
     {
         
     }
+
+	private void FixedUpdate()
+	{
+		currentPos = GetComponent<Transform>().position;
+		Vector2 playerPos= playerRB.position;
+        float horInput = Input.GetAxis("Horizontal");
+        float vertInput = Input.GetAxis("Vertical");
+        Vector2 inputVector = new Vector2(horInput, vertInput);
+        Vector2 movement = inputVector * moveSpeed;
+        Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
+        playerRB.MovePosition(newPos);
+
+	}
 }
