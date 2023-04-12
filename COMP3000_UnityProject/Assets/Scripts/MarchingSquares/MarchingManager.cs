19474910071859d41436e61735fc3e5012759588
Josie Wood <josieeewood@gmail.com>
2023-04-12 12:43:41 +0100

    player pos can be translated to a tile on the tilemap

--- COMP3000_UnityProject/Assets/Scripts/MarchingSquares/MarchingManager.cs
+++ COMP3000_UnityProject/Assets/Scripts/MarchingSquares/MarchingManager.cs
@@ -1,7 +1,9 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.Tilemaps;
 
 public class MarchingManager : MonoBehaviour
@@ -19,15 +21,91 @@ public class MarchingManager : MonoBehaviour
 	public Square[,] squaresArray;
 
 	[SerializeField]
-	public int xSize;
+	public int tilemapXSize;
 	[SerializeField]
-	public int ySize;
+	public int tilemapYSize;
+
+	public GameObject player;
+	public Vector3 playerPosition;
+	public Collider2D forestCollider;
+	public Bounds forestBounds;
+
+	[SerializeField]
+	public int forestXSize;
+	[SerializeField]
+	public int forestYSize;
+
+	public float translationMultiplier;
 
 	private void Start()
 	{
 		generateSquaresArray();
 		generateVerticesArray();
+
+		calculateForestToTilemapMultiplier();
 	}
+
+	private void Update()
+	{
+		// get player position in map
+		playerPosition = player.transform.position;
+
+		translatePlayerPosToTilemap(playerPosition);
+		// translate to the 2d array
+		// find closest vertex
+		// turn vertex on
+	}
+
+	private Vector3Int translatePlayerPosToTilemap(Vector3 playerWorldPosition)
+	{
+		Vector3Int playerTilemapPosition;
+
+
+		
+
+		if (forestXSize/forestYSize != tilemapXSize/tilemapYSize)
+		{
+			Debug.Log("ERROR: the tilemap and forest have different ratios.");
+			playerTilemapPosition = new Vector3Int(0, 0, 0);
+		}
+		else
+		{
+			//tilemap and forest have the same ratios, as required. 
+
+			//multiply playerWorldPos in forest by multiplier to get equivalent vertex on tilemap
+
+			int newPositionX = Mathf.RoundToInt(playerWorldPosition.x / translationMultiplier);
+			int newPositionY = Mathf.RoundToInt(playerWorldPosition.y / translationMultiplier);
+
+			playerTilemapPosition = new Vector3Int(newPositionX, newPositionY, 0);
+		}
+		// tilemapBounds bounds of tilemap
+
+		Debug.Log("Player world pos is " + playerWorldPosition + " and tilemap pos is " + playerTilemapPosition);
+		return playerTilemapPosition;
+		
+	}
+
+	private void calculateForestToTilemapMultiplier()
+	{
+		// takes in the forest and tilemap info and returns the number to multiply one with to get the other
+		// forestPos * multiplier = tilemapPos
+
+		// forestBounds size/bounds of the world space forest
+		forestBounds = forestCollider.bounds;
+		forestXSize = Mathf.RoundToInt(forestBounds.size.x);
+		forestYSize = Mathf.RoundToInt(forestBounds.size.y);
+
+
+		translationMultiplier = (forestXSize / tilemapXSize);
+
+		if(translationMultiplier != (forestYSize/ tilemapYSize)) 
+		{
+			//somethings squiffy - not same ratio
+			Debug.Log("ERROR identifying translation multiplier. Tilemap and Forest space have different ratios.");
+		}
+	}
+
 
 	private void generateSquaresArray()
 	{
@@ -35,18 +113,18 @@ public class MarchingManager : MonoBehaviour
 		tilemap.CompressBounds();
 		tilemapBounds = tilemap.cellBounds;
 
-		Debug.Log("Bounds are " + tilemapBounds);
+
 
 		//create the squares array using the tilemap bounds info to set size
 		//get x size
-		xSize = tilemapBounds.size.x;
-		ySize = tilemapBounds.size.y;
+		tilemapXSize = tilemapBounds.size.x;
+		tilemapYSize = tilemapBounds.size.y;
 
-		squaresArray = new Square[xSize, ySize];
+		squaresArray = new Square[tilemapXSize, tilemapYSize];
 
-		for (int x = 0; x < xSize; x++)
+		for (int x = 0; x < tilemapXSize; x++)
 		{
-			for (int y = 0; y < ySize; y++)
+			for (int y = 0; y < tilemapYSize; y++)
 			{
 				// iterate through every tile within tilemap bounds
 
@@ -62,7 +140,7 @@ public class MarchingManager : MonoBehaviour
 
 					//add to 2darray in size pos
 					squaresArray[x, y] = newSquare;
-					Debug.Log("square at position " + currentPos + " in world was added to position " + x + " , " + y + " in squares array");
+					
 				}
 				else
 				{
@@ -78,13 +156,13 @@ public class MarchingManager : MonoBehaviour
 		// vertices array is always one bigger than the squares array
 		// bc each square has 4 vertices
 
-		verticesArray = new Vertex[xSize + 1, ySize + 1];
-		Debug.Log("new vertices array created with size " + (xSize + 1) + " : " + (ySize + 1));
+		verticesArray = new Vertex[tilemapXSize + 1, tilemapYSize + 1];
+		
 		//iterate through each square, create vertices, add to vertices array if not there, assign to square
 
-		for (int x = 0; x < xSize; x++) 
+		for (int x = 0; x < tilemapXSize; x++) 
 		{
-			for (int y = 0; y < ySize; y++)
+			for (int y = 0; y < tilemapYSize; y++)
 			{
 				Square currentSquare = squaresArray[x, y];
 
